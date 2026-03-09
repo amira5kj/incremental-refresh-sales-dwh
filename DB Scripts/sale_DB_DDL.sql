91f7912605fe3a112695574e1ba94de2f1705516
Amira Reda <110064982+amira5kj@users.noreply.github.com>
2026-03-09 03:33:23 +0200

    Create sale_DB_DDL.sql for all_data table definition

---
+++ DB Scripts/sale_DB_DDL.sql
@@ -0,0 +1,27 @@
+IF OBJECT_ID('all_data', 'U') IS NOT NULL
+    DROP TABLE all_data;
+GO
+
+create table all_data (
+Order_ID       varchar(100),	
+Order_Date     datetime,
+Ship_Date      datetime,
+Ship_Mode	   varchar(50),
+Customer_ID	   varchar(50),
+Customer_Name  varchar(250),
+Segment	       varchar(50),
+Country	       varchar(50),
+City	       varchar(50),
+State	       varchar(50),
+Postal_Code	   varchar(50),
+Region	       varchar(50),
+Product_ID     varchar(250),
+Category	   varchar(50),
+Sub_Category   varchar(50),
+Product_Name   varchar(250),
+Sales          decimal,
+Quantity	   int,
+Discount	   decimal,
+Profit         decimal
+);
+GO
